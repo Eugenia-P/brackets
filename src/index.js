a1aaa9d8567dbb1013391d2785d1a35441f12a49
Eugenia-P <zhenia_niki@mail.ru>
2019-10-09 14:57:06 +0300

    feat: my 55% solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const strArr = str.split(''); 
+
+    function diff(a, b) {
+      return a.filter(function(i) {return a === b;});
+  };
+
+  return diff(strArr, bracketsConfig) == false ? true : false;
 }
