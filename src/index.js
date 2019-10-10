d82700b812f0c84ce9daa862c0f6ccb1f13ac3b9
Eugenia-P <zhenia_niki@mail.ru>
2019-10-10 15:35:19 +0300

    feat: my solution

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  const strArr = str.split(''); 
 
-    function diff(a, b) {
-      return a.filter(function(i) {return a === b;});
-  };
+  let strBrackets = bracketsConfig.join('').split(',').join(''); 
+   
+  for (let i = 0; i < str.length; i++) {
+    let pos = -1; 
 
-  return diff(strArr, bracketsConfig) == false ? true : false;
+    for (let j = 0; j < strBrackets.length; j+=2) {
+      let sample = strBrackets.substr(j, 2); 
+
+      if ((pos = str.indexOf(sample)) !== -1) {
+        str = str.slice(0, pos).concat(str.slice(pos + 2));
+        i = (i === 0) ? i = 0 : i -= 1;
+      }
+    }
+  }
+
+  return str.length === 0 ? true : false; 
 }
